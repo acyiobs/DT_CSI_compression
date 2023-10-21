739c25ad5c0a0048f5bd5577ddbddf935c3bcd70
Shuaifeng Jiang <50979532+acyiobs@users.noreply.github.com>
2023-10-21 01:07:25 -0700

    add simple data selection that selects the data of the largest nmse

--- data_feed/data_feed.py
+++ data_feed/data_feed.py
@@ -8,54 +8,43 @@ from scipy.io import loadmat
 from einops import rearrange, reduce, repeat
 
 
-def create_samples(data_root, csv_path, random_state, num_data_point, portion):
+def create_samples(data_root, csv_path, random_state, num_data_point, portion, select_data_idx):
     # load channel data
     channel_ad_clip = loadmat(data_root+'/channel_ad_clip.mat')['all_channel_ad_clip']
 
     # load data index
-    data_idx = pd.read_csv(data_root+csv_path)["data_idx"].to_numpy()
+    if not select_data_idx:
+        data_idx = pd.read_csv(data_root+csv_path)["data_idx"].to_numpy()
+    else:
+        data_idx = select_data_idx
     channel_ad_clip = channel_ad_clip[data_idx, ...]
 
     # shuffle
     channel_ad_clip, data_idx = sklearn.utils.shuffle(channel_ad_clip, data_idx, random_state=random_state)
     channel_ad_clip = np.squeeze(channel_ad_clip)
 
-    if num_data_point:
-        channel_ad_clip = channel_ad_clip[:num_data_point, ...]
-        data_idx = data_idx[:num_data_point, ...]
-    else:
-        num_data = data_idx.shape[0]
-        p = int(num_data*portion)
+    if not select_data_idx: # if no particular data is selected
+        if num_data_point:
+            channel_ad_clip = channel_ad_clip[:num_data_point, ...]
+            data_idx = data_idx[:num_data_point, ...]
+        else:
+            num_data = data_idx.shape[0]
+            p = int(num_data*portion)
 
-        channel_ad_clip = channel_ad_clip[:p, ...]
-        data_idx = data_idx[:p, ...]
+            channel_ad_clip = channel_ad_clip[:p, ...]
+            data_idx = data_idx[:p, ...]
 
-    # normalization
-    # channel_ad_clip = channel_ad_clip / np.abs(channel_ad_clip).max((-1,-2), keepdims=True)
-
-    # channel_ad_clip = channel_ad_clip / np.linalg.norm(channel_ad_clip,axis=(-1,-2), keepdims=True)
-
-    # channel_ad_clip = channel_ad_clip - np.abs(channel_ad_clip).max((-1,-2), keepdims=True)
-
-    # channel_ad_clip /= np.abs(channel_ad_clip).max((-1,-2), keepdims=True)
     channel_ad_clip /= np.linalg.norm(channel_ad_clip, ord='fro', axis=(-1,-2), keepdims=True)
     channel_ad_clip = channel_ad_clip / np.expand_dims(channel_ad_clip[:, 0, 0] / np.abs(channel_ad_clip[:, 0, 0]), (1,2))
-
-    # channel_ad_clip = torch.from_numpy(channel_ad_clip)
-    # channel_ad_clip = torch.view_as_real(channel_ad_clip)
-    # channel_ad_clip = channel_ad_clip / torch.amax(channel_ad_clip.abs(), dim=(-1,-2,-3), keepdim=True)
-    # channel_ad_clip = torch.view_as_complex(channel_ad_clip).numpy()
-
-    # channel_ad_clip = channel_ad_clip / np.expand_dims(channel_ad_clip[:, 0, 0] / np.abs(channel_ad_clip[:, 0, 0]), (1,2))
 
 
     return channel_ad_clip, data_idx
 
 
 class DataFeed(Dataset):
-    def __init__(self, data_root, csv_path, random_state=0, num_data_point=None, portion=1.0):
+    def __init__(self, data_root, csv_path, random_state=0, num_data_point=None, portion=1.0, select_data_idx=[]):
         self.data_root = data_root
-        self.channel_ad_clip, self.data_idx = create_samples(data_root, csv_path, random_state, num_data_point, portion)
+        self.channel_ad_clip, self.data_idx = create_samples(data_root, csv_path, random_state, num_data_point, portion, select_data_idx)
 
     def __len__(self):
         return len(self.data_idx)
