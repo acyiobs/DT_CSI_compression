1cb973bbd999885d9dbf4ffe6fb69be81384dce3
Shuaifeng Jiang <50979532+acyiobs@users.noreply.github.com>
2023-10-25 01:41:33 -0700

    new result: train on synth and train on real

--- train_model_loop_real.py
+++ train_model_loop_real.py
@@ -9,8 +9,8 @@ from train_model import train_model
 
 if __name__ == "__main__":
     torch.use_deterministic_algorithms(True)
-    real_data_root = "DeepMIMO/DeepMIMO_datasets/Boston5G_3p5_1"
-    synth_data_root = "DeepMIMO/DeepMIMO_datasets/Boston5G_3p5_small_notree"
+    real_data_root = "DeepMIMO/DeepMIMO_datasets/Boston5G_3p5_real"
+    synth_data_root = "DeepMIMO/DeepMIMO_datasets/Boston5G_3p5_notree"
     train_csv = "/train_data_idx.csv"
     val_csv = "/test_data_idx.csv"
     test_csv = "/test_data_idx.csv"
@@ -62,7 +62,7 @@ if __name__ == "__main__":
 
     print(all_avg_nmse)
     savemat(
-        "result4/all_avg_nmse_train_on_real.mat",
+        "result_new_data_1/all_avg_nmse_train_on_real.mat",
         {"all_avg_nmse_train_on_real": all_avg_nmse},
     )
     print("done")
